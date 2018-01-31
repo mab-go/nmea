8e7e4f9533de8ce0e71c90eb556606390c6d46ab
Matt Burton <matthew.a.burton@gmail.com>
2018-01-31 13:07:14 -0500

    Add Travis CI configuration
    This commit adds a config file for the Travis CI continuous integration
    service. Additionally, a Bash script (`tools/test-cover.sh`) was added
    to execute unit tests with code coverage for use with the codecov.io
    service.

---
+++ tools/test-cover.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "" > "coverage.txt"
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic "${d}"
+    if [[ -f "profile.out" ]]; then
+        cat "profile.out" >> "coverage.txt"
+        rm "profile.out"
+    fi
+done
