4d7ae44e50c75f176320bd42d5ba4c4273e04775
Matt Burton <matthew.a.burton@gmail.com>
2018-02-01 11:37:24 -0500

    Remove coverage.txt from version control

--- tools/test-cover.sh
+++ tools/test-cover.sh
@@ -7,7 +7,7 @@ set -o pipefail
 echo "" > "coverage.txt"
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic "${d}"
+    go test -race -coverprofile="profile.out" -covermode=atomic "${d}"
     if [[ -f "profile.out" ]]; then
         cat "profile.out" >> "coverage.txt"
         rm "profile.out"
